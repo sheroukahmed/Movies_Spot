7548a3cc51eebe1edce3fdc867f3c844c00e2f41
sherouk ahmed <shroukhelal2010@gmail.com>
2024-09-26 22:47:43 +0300

    adding api manger class

--- banquemisr.challenge05/Services/Remote/API Manger/APIManger.swift
+++ banquemisr.challenge05/Services/Remote/API Manger/APIManger.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class APIManger {
+    
+    static let base = "https://api.themoviedb.org/3/movie/"
+    
+    enum endpoints : String{
+        case upcomming = "upcoming"
+        case nowplaying = "now_playing"
+        case popular = "popular"
+    }
+    
+    
+    static func getFullURL(details: String , movieID : Int = 0) -> String? {
+        switch details {
+        case endpoints.upcomming.rawValue:
+            return base + endpoints.upcomming.rawValue
+        case endpoints.popular.rawValue:
+            return base + endpoints.popular.rawValue
+        case endpoints.nowplaying.rawValue:
+            return base + endpoints.nowplaying.rawValue
+        case "movie":
+            return base + String(movieID) 
+        default:
+            return base + endpoints.upcomming.rawValue
+        }
+        
+    }
+}
